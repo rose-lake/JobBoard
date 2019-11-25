5e864334369a226d8faddaf1414e03cb1b3af9d7
rose-lake <klake12@montgomerycollege.edu>
2019-11-25 09:09:34 -0500

    first commit

---
+++ src/test/java/com/roselake/jbc/CraigsJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roselake.jbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraigsJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
